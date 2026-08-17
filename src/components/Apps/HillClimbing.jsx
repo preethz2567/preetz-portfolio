fb2308bf52d7bc9b33e394198cfa6e692e2d057d
Preethi Durgaprasad <preethidurgaprasad@gmail.com>
2026-08-17 11:34:37 +0530

    Update Hill Climbing embed to use GameDistribution URL for reliability

--- src/components/Apps/HillClimbing.jsx
+++ src/components/Apps/HillClimbing.jsx
@@ -7,7 +7,7 @@ const HillClimbing = ({ isMaximized }) => {
       style={{ overflow: "hidden", touchAction: "none" }}
     >
       <iframe 
-        src="https://www.crazygames.com/embed/moto-x3m"
+        src="https://html5.gamedistribution.com/5b0abd4c0faa4f5eb190a9a16d5a1b4c/?gd_sdk_referrer_url=https://github.com/preethz2567/preetz-portfolio"
         width="100%" 
         height="100%" 
         scrolling="none" 
