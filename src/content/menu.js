82a3dc822c99a3cbd9031f90c6405402f0d9ef97
Preethi Durgaprasad <preethidurgaprasad@gmail.com>
2026-08-17 11:05:11 +0530

    Add Minesweeper to menu

--- src/content/menu.js
+++ src/content/menu.js
@@ -90,4 +90,11 @@ export const Menu = [
         headerIconSize: "h-[22px] w-[22px]",
         startIconSize: "w-[22px] h-[22px]"
     },
+    {
+        name: "Minesweeper.exe",
+        path: "/assets/projects.png", // We can use projects.png or find a bomb icon later
+        deskIconSize: "w-[42px] h-[42px]",
+        headerIconSize: "h-[22px] w-[22px]",
+        startIconSize: "w-[22px] h-[22px]"
+    }
 ];
